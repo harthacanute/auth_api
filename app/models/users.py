338ba734cacee1dcc5ad5d2f51e35dba46c2c862
motolani <62948567+harthacanute@users.noreply.github.com>
2026-07-18 16:18:44 +0000

    Users model done

--- app/models/users.py
+++ app/models/users.py
@@ -1,6 +1,19 @@
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from sqlalchemy.dialects.postgresql import UUID
-from uuid import uuid4
+from uuid import UUID as pyUUID
 from datetime import datetime
 from ..database import Base
 from sqlalchemy import String, DateTime, ForeignKey, text
+
+class User(Base):
+    __tablename__ = "users"
+
+    id: Mapped[pyUUID] = mapped_column(UUID(as_uuid=True), primary_key=True, server_default= text("gen_random_uuid()"))
+    email: Mapped[str] = mapped_column(String(255), unique=True, nullable=False, index = True)
+    hashed_password: Mapped[str | None] = mapped_column(String(60), nullable=True)
+    is_active: Mapped[bool] = mapped_column(nullable=False, default=True)
+    is_verified: Mapped[bool] = mapped_column(nullable=False, default=False)
+    mfa_enabled: Mapped[bool] = mapped_column(nullable=False, default=False)
+    mfa_secret: Mapped[str | None] = mapped_column(String(255), nullable=True)
+    created_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), server_default=text("now()"))
+    updated_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), onupdate=datetime.timezone.utcnow, server_default=text("now()"))
